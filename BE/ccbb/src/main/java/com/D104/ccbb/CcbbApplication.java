03a5c3ec1862e87f078a4afb2a89d69c1cc5b4d8
최영창 <choiyc1446@gmail.com>
2023-10-19 09:39:07 +0900

    BE init commit

---
+++ BE/ccbb/src/main/java/com/D104/ccbb/CcbbApplication.java
@@ -0,0 +1,13 @@
+package com.D104.ccbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbbApplication.class, args);
+	}
+
+}
---
+++ BE/ccbb/src/test/java/com/D104/ccbb/CcbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D104.ccbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
