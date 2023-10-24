f80ecd58e0a4723d5960f0ce56a4bbead497e953
신경희 <skh1615@gmail.com>
2023-10-24 15:16:55 +0900

    feat: 버튼하단메뉴추가

--- FE/ccbb/src/pages/userPages/signinPage/style.js
+++ FE/ccbb/src/pages/userPages/signinPage/style.js
@@ -4,4 +4,15 @@ export const bg = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: darkslateblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const signinMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  text-decoration: none;
+  color: white;
+  background-color: green;
 `;
