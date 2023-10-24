97de90432de6750d395e14da1a46654eed90a394
박종욱 <pju7429@naver.com>
2023-10-24 13:02:31 +0900

    feat: 컴포넌트 추가

--- FE/ccbb/src/component/buttons/index.jsx
+++
@@ -1,7 +0,0 @@
-import * as S from './style';
-
-export default function Button1({text,width,height}){
-    return (
-        <S.Button1 width={width} height={height}>{text}</S.Button1>
-    )
-}
---
+++ FE/ccbb/src/pages/userPages/signinPage/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './style';
+
+export default function SigninPage() {
+    return (
+        <>
+            <h1>로그인 페이지</h1>
+        </>
+        
+    )
+}
