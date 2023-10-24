a032c2afa59006b0da459bac75019957204378f9
신경희 <skh1615@gmail.com>
2023-10-24 17:40:47 +0900

    Feat: 로그인 인풋에 state바인딩, keyPress 적용

--- FE/ccbb/src/pages/userPages/signinPage/index.jsx
+++ FE/ccbb/src/pages/userPages/signinPage/index.jsx
@@ -1,11 +1,24 @@
+import React, { useState } from "react";
 import * as S from "./style";
 import Input1 from "../../../component/common/inputs/input1";
 import Button1 from "../../../component/common/buttons";
 import { Link } from "react-router-dom";
 
 export default function SigninPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const doLogin = () => {
     console.log("로그인합시다");
+    console.log("id: " + email);
+    console.log("pw: " + password);
+  };
+  const handleOnKeyPress = (e) => {
+    if (e.key === "Enter") {
+      //이메일, 패스워드 중 하나라도 비어있으면 리턴시킴
+      console.log("엔터키입력");
+      doLogin(); // Enter 입력이 되면 클릭 이벤트 실행
+    }
   };
   return (
     <S.bg>
@@ -14,13 +27,28 @@ export default function SigninPage() {
         alt="CC.BB"
         style={{ width: "200px", height: "60px" }}
       />
-      <Input1 label="이메일" id="idInput" width="300px" height="40px" />
+      <Input1
+        label="이메일"
+        id="idInput"
+        width="300px"
+        height="40px"
+        onKeyPress={handleOnKeyPress}
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
       <Input1
         label="패스워드"
         id="passwordInput"
         width="300px"
         height="40px"
         type="password"
+        onKeyPress={handleOnKeyPress}
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
       />
       <Button1
         text={"로그인"}
--- FE/ccbb/src/style.js
+++ FE/ccbb/src/style.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import { theme } from "./styles/Theme";
 
 export const main = styled.main`
-  background-color :${theme.mainbg};
+  background-color: ${theme.mainbg};
   width: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+  min-height: 100vh;
+`;
