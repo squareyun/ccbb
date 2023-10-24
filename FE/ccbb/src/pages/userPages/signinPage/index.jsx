191f0fe395af33f137e1a216c7c716321e8da69e
신경희 <skh1615@gmail.com>
2023-10-24 14:06:56 +0900

    feat: 인풋, 버튼추가(임시 bg 제거예정)

--- FE/ccbb/src/pages/userPages/signinPage/index.jsx
+++ FE/ccbb/src/pages/userPages/signinPage/index.jsx
@@ -1,10 +1,20 @@
-import * as S from './style';
+import * as S from "./style";
+import Input1 from "../../../component/common/inputs/input1";
+import Button1 from "../../../component/common/buttons";
 
 export default function SigninPage() {
-    return (
-        <>
-            <h1>로그인 페이지</h1>
-        </>
-        
-    )
+  return (
+    <S.bg>
+      <h1>시시비비</h1>
+      <Input1 label="아이디" id="idInput" width="300px" height="40px" />
+      <Input1
+        label="비밀번호"
+        id="passwordInput"
+        width="300px"
+        height="40px"
+        type="password"
+      />
+      <Button1 text={"로그인"} width={"75px"} height={"30px"} />
+    </S.bg>
+  );
 }
--- FE/ccbb/src/pages/userPages/signinPage/style.js
+++ FE/ccbb/src/pages/userPages/signinPage/style.js
@@ -1,1 +1,7 @@
 import styled from "styled-components";
+
+export const bg = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: darkslateblue;
+`;
