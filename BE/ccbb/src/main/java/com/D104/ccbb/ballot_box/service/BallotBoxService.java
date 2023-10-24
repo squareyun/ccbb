a00cb9a6f36f3d55dc7ca76d4a5d016c184f38a1
채경호 <corudgh123@naver.com>
2023-10-24 17:51:25 +0900

    feat: BallotBox, ReComment 일부 구현

--- BE/ccbb/src/main/java/com/D104/ccbb/ballot_box/service/BallotBoxService.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/ballot_box/service/BallotBoxService.java
@@ -3,10 +3,31 @@ package com.D104.ccbb.ballot_box.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.D104.ccbb.ballot_box.domain.BallotBox;
+import com.D104.ccbb.ballot_box.dto.BallotBoxDto;
+import com.D104.ccbb.ballot_box.repo.BallotBoxRepo;
+import com.D104.ccbb.user.repository.UserRepository;
+import com.D104.ccbb.vote.repo.VoteRepo;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BallotBoxService {
+
+	private final BallotBoxRepo ballotBoxRepo;
+	private final UserRepository userRepository;
+	private final VoteRepo voteRepo;
+
+	public void setBallotBox(BallotBoxDto ballotBoxDto) {
+		BallotBox ballotBox = BallotBox.builder()
+			.ballotBoxId(ballotBoxDto.getBallotBoxId())
+			.pick(ballotBoxDto.getPick())
+			.userId(userRepository.getReferenceById(ballotBoxDto.getUserId()))
+			.voteId(voteRepo.getReferenceById(ballotBoxDto.getVoteId()))
+			.build();
+		ballotBoxRepo.save(ballotBox);
+	}
+
 }
--- BE/ccbb/src/main/java/com/D104/ccbb/re_comment/repo/ReCommentRepo.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/re_comment/repo/ReCommentRepo.java
@@ -1,8 +1,13 @@
 package com.D104.ccbb.re_comment.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.D104.ccbb.re_comment.domain.ReComment;
 
+@Repository
 public interface ReCommentRepo extends JpaRepository<ReComment, Integer> {
+	List<ReComment> findByCommentId_CommentId(int commentId);
 }
