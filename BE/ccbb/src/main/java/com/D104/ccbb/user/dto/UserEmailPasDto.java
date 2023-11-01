159c155f6543a590d56a19dafcafdff06aaebdac
DESKTOP-DOGVPUB\SSAFY <corudgh123@naver.com>
2023-11-01 17:46:20 +0900

    feat: 통계값 산출

--- BE/ccbb/src/main/java/com/D104/ccbb/user/dto/UserEmailPasDto.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/user/dto/UserEmailPasDto.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEmailPasDto {
-	private String email;
-	private String password;
+	 String email;
+	 String password;
 }
--- BE/ccbb/src/main/java/com/D104/ccbb/user/repository/UserRepository.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.D104.ccbb.user.repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,6 +20,27 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	// 메소드 이름 변경
 	Optional<User> findUserByEmail(String email);
+
+	@Query(value = "select u.lol_tier as lolTier, sum(u.vote_count) as voteCount, sum(u.vote_victory) as voteVictory " +
+		"from User u " +
+		"group by u.lol_tier",nativeQuery = true)
+	List<Map<String, Object>> getStatisticsTier();
+
+	@Query(value = "select u.main_position as position, sum(u.vote_count) as voteCount, sum(u.vote_victory) as voteVictory " +
+		"from User u " +
+		"group by u.main_position",nativeQuery = true)
+	List<Map<String, Object>> getStatisticsPosition();
+
+	@Query(value = "select u.lol_tier as lolTier, count(*) as userCount "+
+		"from User u " +
+		"group by u.lol_tier",nativeQuery = true)
+	List<Map<String, Object>> getUserCountTier();
+
+
+	@Query(value = "select u.main_position as lolPosition, count(*) as userCount "+
+		"from User u " +
+		"group by u.main_position",nativeQuery = true)
+	List<Map<String, Object>> getUserCountPosition();
 
 	@Modifying
 	@Transactional
