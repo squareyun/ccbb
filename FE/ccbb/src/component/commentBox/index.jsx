3413c679b1548b157b520724b34e346eaead92d5
신경희 <skh1615@gmail.com>
2023-11-07 20:43:37 +0900

    feat: 댓글삭제 수정중
    Former-commit-id: 943148a8762cff630aaf8151e8f8bdc98291ebca

--- FE/ccbb/src/component/commentBox/index.jsx
+++ FE/ccbb/src/component/commentBox/index.jsx
@@ -1,21 +1,41 @@
-import Button1 from '../common/buttons';
-import UserProfile from '../common/profile';
-import * as S from './style';
+import UserProfile from "../common/profile";
+import * as S from "./style";
 
-export default function CommentBox({ bgcolor, comment, userId, date }) {
+export default function CommentBox({
+  isMine = false,
+  bgcolor,
+  comment,
+  nickname,
+  date,
+  position,
+  onClickModify,
+  onClickDelete,
+}) {
   return (
     <S.Box bgcolor={bgcolor}>
       <S.CommentHead>
-        <UserProfile name={userId} color={'black'} iconSize={'30px'} fontsize={'15px'}/>
+        <UserProfile
+          name={nickname}
+          color={"black"}
+          iconSize={"30px"}
+          fontsize={"15px"}
+        />
         <p>{date}</p>
       </S.CommentHead>
       <S.CommentBody>
         <p>{comment}</p>
       </S.CommentBody>
-      <S.CommentBottom>
-        <Button1 text={'수정'} height={'30px'} width={'50px'} fontsize={'15px'} />
-        <Button1 text={'삭제'} color={'red'} height={'30px'} width={'50px'} fontsize={'15px'}/>
-      </S.CommentBottom>
+      {isMine && (
+        <S.CommentBottom>
+          <span className="modify-comment" onClick={onClickModify}>
+            수정
+          </span>
+          <span className="divider">|</span>
+          <span className="delete-comment" onClick={onClickDelete}>
+            삭제
+          </span>
+        </S.CommentBottom>
+      )}
     </S.Box>
   );
 }
