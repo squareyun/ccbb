162e5ff18f37efab11164afd4a8960df27612df4
최영창 <choiyc1446@gmail.com>
2023-11-07 17:49:53 +0900

    fix: 댓글 lazy 에러 해결 시도
    Former-commit-id: 72cb310a2c60dd944e88e333beb9e4795df807bf

--- BE/ccbb/src/main/java/com/D104/ccbb/comment/repo/CommentRepo.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/comment/repo/CommentRepo.java
@@ -1,16 +1,20 @@
 package com.D104.ccbb.comment.repo;
 
-import com.D104.ccbb.comment.domain.Comment;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.D104.ccbb.comment.domain.Comment;
 
 @Repository
 public interface CommentRepo extends JpaRepository<Comment, Integer> {
-    @EntityGraph(attributePaths = {"reComment", "userId"})
-    List<Comment> findByPostId_PostId(int postId);
+	@EntityGraph(attributePaths = {"reComment", "userId"})
+	List<Comment> findByPostId_PostId(int postId);
 
-    Long countByPostId_PostId(int postId);
+	Long countByPostId_PostId(int postId);
+
+	@EntityGraph(attributePaths = {"reComment", "userId"})
+	void delete(int commentId);
 }
