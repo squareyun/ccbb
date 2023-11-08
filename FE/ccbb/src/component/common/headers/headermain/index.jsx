3e6f5a83a489e3e816259c814a3bafc4742587ab
윤우혁 <squareyun@gmail.com>
2023-11-08 14:06:50 +0900

    fix: 알림 빨간 점 문제 해결
    Former-commit-id: 2e5e94df0063f692a45f49d3fef6ab774bda97a1

--- FE/ccbb/src/component/common/headers/headermain/index.jsx
+++ FE/ccbb/src/component/common/headers/headermain/index.jsx
@@ -45,7 +45,6 @@ export default function Headermain() {
     ccbbApi
       .get("/notification/get", { headers })
       .then((res) => {
-        // console.log(res);
         setNotifications(res.data.notificationList);
       })
       .catch((e) => console.log(e));
@@ -95,7 +94,7 @@ export default function Headermain() {
         // console.log("closed");
       }
     };
-  }, []);
+  }, [localStorage.getItem("token")]);
 
   return (
     <S.Header>
@@ -154,11 +153,7 @@ export default function Headermain() {
               >
                 <path d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"></path>
               </svg>
-              <S.RedDot
-                isvisible={notifications
-                  .some((notification) => !notification.isRead)
-                  .toString()}
-              />
+              <S.RedDot $isvisible={notifications.some(notification => !notification.isRead)} />
               {notificationVisible && (
                 <Headernotification
                   props={{
