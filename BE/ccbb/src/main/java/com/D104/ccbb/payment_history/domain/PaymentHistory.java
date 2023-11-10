a7df42ea69ce636d40c1088064a31e6c052a3c21
윤우혁 <squareyun@gmail.com>
2023-11-10 17:16:41 +0900

    fix: 투표 삭제 시 무한루프 문제 해결
    Former-commit-id: 586eea3e8f452cd39bb164250982c8d2ebffa588

--- BE/ccbb/src/main/java/com/D104/ccbb/payment_history/domain/PaymentHistory.java
+++ BE/ccbb/src/main/java/com/D104/ccbb/payment_history/domain/PaymentHistory.java
@@ -1,22 +1,11 @@
 package com.D104.ccbb.payment_history.domain;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import com.D104.ccbb.user.domain.User;
 import com.D104.ccbb.vote.domain.Vote;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -24,34 +13,36 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class PaymentHistory {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "history_id", nullable = false)
-	private Integer historyId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "history_id", nullable = false)
+    private Integer historyId;
 
-	@Column(nullable = false, columnDefinition = "int")
-	private Integer amount;
+    @Column(nullable = false, columnDefinition = "int")
+    private Integer amount;
 
-	@Column(name = "is_returned", nullable = false)
-	private Boolean isReturned;
+    @Column(name = "is_returned", nullable = false)
+    private Boolean isReturned;
 
-	@Column(name = "pay_date", nullable = false, columnDefinition = "DATETIME")
-	private LocalDateTime payDate;
+    @Column(name = "pay_date", nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime payDate;
 
-	@ManyToOne
-	@JoinColumn(name = "user_id", nullable = false)
-	private User userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
+    private User userId;
 
-	@ManyToOne
-	@JoinColumn(name = "vote_id", nullable = false)
-	private Vote voteId;
+    @ManyToOne
+    @JoinColumn(name = "vote_id", nullable = false)
+    @ToString.Exclude
+    private Vote voteId;
 
-	@Column(name = "tid", nullable = false)
-	private String tid;
+    @Column(name = "tid", nullable = false)
+    private String tid;
 
-	@Column(name = "is_approve", nullable = false)
-	private boolean isApprove;
+    @Column(name = "is_approve", nullable = false)
+    private boolean isApprove;
 
-	@Column(name = "partner_order_id", nullable = false)
-	private String partnerOrderId;
+    @Column(name = "partner_order_id", nullable = false)
+    private String partnerOrderId;
 }
