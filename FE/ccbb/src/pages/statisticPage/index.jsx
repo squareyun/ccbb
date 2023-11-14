084a9d6b5b2aad778d5cc06e4aae55f98e8ce670
신경희 <skh1615@gmail.com>
2023-11-14 15:40:55 +0900

    feat: 통계페이지 제티차트 반영 Todo: 라벨, 컬러, css 수정
    Former-commit-id: 89f6338e5887c903e0e7df4d6c165318a280976d

--- FE/ccbb/src/pages/statisticPage/index.jsx
+++ FE/ccbb/src/pages/statisticPage/index.jsx
@@ -1,8 +1,8 @@
 import * as S from "./style";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Headermenu from "../../component/common/headers/headermenu";
 import { ccbbApi } from "../../api/ccbbApi";
-// import { NormalMap, NormalScatter } from "jetty-chart";
+import { Pie } from "jetty-chart";
 
 export default function StatisticPage() {
   const [statisticsErr, setStatisticsErr] = useState(false);
@@ -19,25 +19,25 @@ export default function StatisticPage() {
         ccbbApi
           .get("/statistics/count/tier")
           .then((res) => {
-            setTierDist(res.data.statistics);
+            setTierDist(makeData(res.data.statistics));
           })
           .catch((e) => console.log(e));
         ccbbApi
           .get("/statistics/count/position")
           .then((res) => {
-            setPositionDist(res.data.statistics);
+            setPositionDist(makeData(res.data.statistics));
           })
           .catch((e) => console.log(e));
         ccbbApi
           .get("/statistics/vote/tier")
           .then((res) => {
-            setWinRateByTier(res.data.statistics);
+            setWinRateByTier(makeData(res.data.statistics));
           })
           .catch((e) => console.log(e));
         ccbbApi
           .get("/statistics/vote/position")
           .then((res) => {
-            setWinRateByPosition(res.data.statistics);
+            setWinRateByPosition(makeData(res.data.statistics));
           })
           .catch((e) => console.log(e));
       })
@@ -46,6 +46,38 @@ export default function StatisticPage() {
         console.log(e);
       });
   }, []);
+
+  function numToPosition(num) {
+    switch (num) {
+      case 0:
+        return "탑";
+      case 1:
+        return "정글";
+      case 2:
+        return "미드";
+      case 3:
+        return "원딜";
+      case 4:
+        return "서폿";
+      default:
+        return "";
+    }
+  }
+  const makeData = (arr) => {
+    return arr
+      .map((item) => {
+        let label = item.lolTier || numToPosition(item.lolPosition);
+        let value = item.userCount || item.voteVictory / item.voteCount;
+        if (label && value) {
+          return {
+            label: label,
+            value: value,
+          };
+        }
+      })
+      .filter((item) => item !== undefined);
+  };
+
   return (
     <S.main>
       <Headermenu title="통계" />
@@ -56,9 +88,37 @@ export default function StatisticPage() {
           </h2>
         )}
         <h2>유저 티어 분포</h2>
+        <Pie
+          data={tierDist}
+          generalSettings={{
+            backgroundColor: "white",
+          }}
+          pieSettings={{ innerRadius: 0.5, cornerRadius: 0.04, padAngle: 2 }}
+        />
         <h2>유저 포지션 분포</h2>
-        <h2>티어별 투표 승리율</h2>
-        <h2>포지션별 투표 승리율</h2>
+        <Pie
+          data={positionDist}
+          generalSettings={{
+            backgroundColor: "white",
+          }}
+          pieSettings={{ innerRadius: 0.5, cornerRadius: 0.04, padAngle: 2 }}
+        />
+        <h2>티어별 투표 승률</h2>
+        <Pie
+          data={winRateByTier}
+          generalSettings={{
+            backgroundColor: "white",
+          }}
+          pieSettings={{ innerRadius: 0.5, cornerRadius: 0.04, padAngle: 2 }}
+        />
+        <h2>포지션별 투표 승률</h2>
+        <Pie
+          data={winRateByPosition}
+          generalSettings={{
+            backgroundColor: "white",
+          }}
+          pieSettings={{ innerRadius: 0.5, cornerRadius: 0.04, padAngle: 2 }}
+        />
       </S.statisticSection>
     </S.main>
   );
